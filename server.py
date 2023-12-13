754594e8f038078401fbae9ec7613ce8f8eb8bd5
riziuzi <rishi18december@gmail.com>
2023-12-13 19:38:42 +0530

    nothing special, jsut testing mongo Atlas

--- server.py
+++ server.py
@@ -1,21 +1,11 @@
-from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/task_manager'
-mongo = PyMongo(app)
-
-@app.route('/tasks', methods=['GET'])
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    result = []
-    for task in tasks:
-        result.append({
-            'id': str(task['_id']),
-            'title': task['title'],
-            'description': task['description']
-        })
-    return jsonify(result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://riziuzi:<password>@riziuzicluster.ulcokkb.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
