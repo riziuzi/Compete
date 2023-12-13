616e80a91bcbb4a6df66c53bcffa7079f08aa0a8
rishi <rishi18december@gmail.com>
2023-12-14 01:14:51 +0530

    progressing

--- server/server.py
+++ server/server.py
@@ -7,11 +7,32 @@ app = Flask(__name__)
 
 uri = "mongodb+srv://riziuzi:GClDGPPK1AZwCcEP@riziuzicluster.ulcokkb.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri)
+db = client.compete_database
+
+@app.route("/api/tasks/<str:user_id", method = ["GET"])
+def get_data(user_id):
+    task_collection = db[f"{user_id}_collection"]
+    data = list(task_collection.find())  # Convert cursor to a list of documents
+    return jsonify(data)
+
+# @app.route("/api/tasks", method=["POST"])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 task_no = 1
 message = "Hello tha dhaba"
 
-db = client.compete_database
 usr_name = "rishiSIR"
 task_collection = db[f"{usr_name}_collection"]
 
@@ -22,4 +43,6 @@ task_document = {
 }
 
 post_id = task_collection.insert_one(task_document).inserted_id
-print(post_id)
+post_id = task_collection.find()
+for i in post_id:
+    print(i)
--- src/Pages/JournalAI.js
+++ src/Pages/JournalAI.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../Components/Navbar'
 import Task from '../Components/Task'
 import Footer from '../Components/Footer'
@@ -7,6 +7,18 @@ export default function JournalAI() {
 
   const [numberofTasks, setnumberofTasks] = useState()
   const [tasks, setTasks] = useState([])
+  const userID = "rishi"    // must be unique
+  useEffect(() => {
+    fetch("/api/tasks/${userID}").then(
+      res => res.json()
+    ).then(
+      data => {
+        setTasks(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
     <>
         <Navbar />
