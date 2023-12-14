309d664dcb41701bfabf6e04136c1397e5759aa2
rishi <rishi18december@gmail.com>
2023-12-15 01:44:01 +0530

    Confused in how to use the .env file with the current ip address of my pc assigned in REACT_APP_HOST variable from react component. So, quiting it at just updating the .env file with current ip and port number whenever the server.py starts.

--- server/server.py
+++ server/server.py
@@ -2,11 +2,41 @@ import datetime
 from flask import Flask, request, jsonify
 from pymongo.mongo_client import MongoClient
 from flask_cors import CORS  # Import the CORS extension
+from dotenv import load_dotenv
+import os
+import socket
 
-host = "192.168.1.7"  # Replace with your machine's IP address
-port = 5000  # You can choose any available port
+load_dotenv()
+
+def get_ip_address():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+
+        return ip_address
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def updateEnv():
+    ip_address_str = str(get_ip_address())
+
+    # os.environ["HOST"] = ip_address_str
+    # os.environ["PORT"] = "8088"
+
+    with open(".env", "w") as env_file:
+        env_file.write(f"REACT_APP_HOST={ip_address_str}\n")
+        env_file.write("REACT_APP_PORT=8088\n")
+
+    print(f"HOST value updated to: {ip_address_str}")
+    print("PORT value updated to: 8088")
+    
+
 app = Flask(__name__)
 CORS(app)
+updateEnv()
 
 uri = "mongodb+srv://riziuzi:GClDGPPK1AZwCcEP@riziuzicluster.ulcokkb.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri)
@@ -49,7 +79,18 @@ def submit_task():
 
 
 
-app.run(debug=True, host=host, port=port)
+app.run(debug=True, host=os.getenv('HOST',"192.168.1.6"), port=os.getenv('PORT', 8055))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/Pages/JournalAI.js
+++ src/Pages/JournalAI.js
@@ -3,13 +3,15 @@ import Navbar from '../Components/Navbar'
 import Task from '../Components/Task'
 import Footer from '../Components/Footer'
 import OlderTask from '../Components/OlderTask'
-import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+
 export default function JournalAI() {
 
+  // const apiUrl = process.env.REACT_APP_HOST ??????????????????????????????????????????????????????
+  // console.log(apiUrl)
   const [tasks, setTasks] = useState([])
   const userID = "rishiSIR"    // must be unique
   useEffect(() => {
-    fetch("http://192.168.1.7:5000/api/tasks/"+userID).then(
+    fetch("http://192.168.1.6:8055/api/tasks/"+userID).then(
       res => res.json()
     ).then(
       data => {
@@ -42,3 +44,12 @@ export default function JournalAI() {
 
 // 1) task no. will be give here in React, not in server or faDatabase
 // 2) read https://stackoverflow.com/questions/74634082/connect-to-localhost-from-mobile-phone-using-express-nodejs
+// 3) make a static IP address, which is used by the server.py to run, and never changes (because though 0.0.0.0 opens all network interfaces, 
+// but the ip address to access the server still changes, since it is machine ip iteself), 
+// right now I am jsut assigning 192... by myself, but "If you're running this in a production environment, 
+// you might want to consider more robust solutions, such as using a static IP address, Dynamic DNS, or containerization, 
+// depending on your specific requirements. For development purposes within a controlled environment, manually assigning the 
+// IP address as you've done is a straightforward approach.""
+//
+//
+// 4) WHy this file is not getting the value of REACT_APP_HOST already set in the .env file, I dont know
