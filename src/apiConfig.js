b3aa1c1e4ebdc314d6505c0ff8b2268c65f12889
rishi <rishi18december@gmail.com>
2024-01-17 04:01:38 +0530

    apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
-const zerop0 = "https://zerop0-0xr2.onrender.com"
-const onep0 = "https://onep0.onrender.com"
-const twop0 = "https://twop0.onrender.com"
-const threep0 = "https://threep0.onrender.com"
+export const zerop0 = "https://zerop0-0xr2.onrender.com"
+export const onep0 = "https://onep0.onrender.com"
+export const twop0 = "https://twop0.onrender.com"
+export const threep0 = "https://threep0.onrender.com"
