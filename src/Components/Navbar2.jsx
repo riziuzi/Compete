c1be2869c07c6ff92e455ac646b79c1100124487
rishi <rishi18december@gmail.com>
2024-01-18 22:54:06 +0530

    emplying transitions through tailwind

--- src/Components/Navbar2.jsx
+++ src/Components/Navbar2.jsx
@@ -5,12 +5,13 @@ export default function Navbar2() {
     const [handle, sethandle] = useState()
     const { authenticated, loading, userObj } = useAuthenticate()
     const renderNavItem = (text, link) => (
-        <div className="navItem ml-1 flex-1">
-            <a className="navLink mr-5 text-base text-skin-text200" href={link}>
-                {text}
-            </a>
-        </div>
-    );
+            <div className="navItem flex items-center px-3 py-2 justify-center hover:shadow-md hover:bg-skin-primary200 rounded-lg transition ease-in-out  duration-100 mx-5">
+                <a className="navLink  text-base text-center font-bold text-skin-text200 whitespace-nowrap w-full justify-center flex" href={link}>
+                    {text}
+                </a>
+            </div>
+        );
+
 
     return (
         <div className="header sticky top-0 px-5 bg-skin-bg200 flex justify-between items-center w-full h-14">
@@ -18,14 +19,14 @@ export default function Navbar2() {
                 <img className='hover:cursor-pointer' src="./img/dark.svg" alt="Logo" />
             </a>
             <div className="items w-1/3 flex">
-            {renderNavItem("Home", "/welcome")}
-            {renderNavItem("JournalAI", "/JournalAI")}
-            {renderNavItem("Resources", "/resource")}
-            {loading ? renderNavItem('...Loading', '/profile') :
-                authenticated ? renderNavItem(userObj.userId, '/profile') :
-                    renderNavItem('Login', '/signin')
-            }
-            {renderNavItem("About", "/about")}
+                {renderNavItem("Home", "/welcome")}
+                {renderNavItem("JournalAI", "/JournalAI")}
+                {renderNavItem("Resources", "/resource")}
+                {loading ? renderNavItem('...Loading', '/profile') :
+                    authenticated ? renderNavItem(userObj.userId, '/profile') :
+                        renderNavItem('Login', '/signin')
+                }
+                {renderNavItem("About", "/about")}
             </div>
 
         </div>
